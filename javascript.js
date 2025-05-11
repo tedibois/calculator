f2359dc3154efe4b38ad9879fd161a1e2a5dd9b7
dib <60963856+tedibois@users.noreply.github.com>
2025-05-11 16:58:45 +0200

    .

--- javascript.js
+++ javascript.js
@@ -12,7 +12,11 @@ function add (...theArgs){
 }
 function subtract (...theArgs) {
     if (theArgs.length === 0) return 0;
-
+    let total = theArgs[0];
+    for (let i = 1; i < theArgs.length; i++){
+        total -= theArgs[i];
+    }
+    return total
 }
 function multiply (...theArgs) {
     if (theArgs.length === 0) return 1;
@@ -23,7 +27,40 @@ function multiply (...theArgs) {
     return total
 }
 function divide (...theArgs) {
-    return a / b;
+    if (theArgs.length === 0) return 1;
+    let total = theArgs[0];
+    for (let i = 1; i < theArgs.length; i++){
+        total /= theArgs[i];
+    }
+    return total
 }
 console.log(add(10,5,5,10))
+console.log(subtract(10,5,5,10))
 console.log(multiply(10,5,5,10))
+console.log(divide(10,5,5,10))
+function operate(operator, a, b) {
+    if (operator === "+") return add(a,b);
+    if (operator === "-") return subtract(a,b);
+    if (operator === "*") return multiply(a,b);
+    if (operator === "/") return divide(a,b);
+}
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = null;
+let resultDisplayed = false;
+const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll(".number")
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+    const digit = button.textContent;
+
+    if (currentOperator === null) {
+        firstNumber += digit;
+        display.textContent = firstNumber;
+    } else {
+    secondNumber += digit;
+    display.textContent = secondNumber;
+    }
+});
+});
+const operatorButtons = document.querySelectorAll(".operator");
